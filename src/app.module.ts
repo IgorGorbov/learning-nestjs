1b199019577d809ab4bb28bdacdd17b0abd57452
Igor Gorbov <gorbov.dev@gmail.com>
2022-01-20 22:55:52 +0300

    feat: Add ConfigService to TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,38 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import config from '@app/db.config';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
 import { UserModule } from '@app/user/user.module';
 import { AuthMiddleware } from '@app/user/middlewares/auth.middleware';
 import { ArticleModule } from '@app/article/article.module';
+import { TagEntity } from '@app/tag/tag.entity';
+import { UserEntity } from '@app/user/user.entity';
+import { ArticleEntity } from '@app/article/article.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env.development',
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRoot(config),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          database: configService.get('DATABASE_NAME'),
+          host: configService.get('DATABASE_HOST'),
+          username: configService.get('DATABASE_USER'),
+          password: configService.get('DATABASE_PASSWORD'),
+          port: 5432,
+          entities: [TagEntity, UserEntity, ArticleEntity],
+          synchronize: process.env.NODE_ENV === 'dev',
+        };
+      },
+    }),
     TagModule,
     UserModule,
     ArticleModule,
